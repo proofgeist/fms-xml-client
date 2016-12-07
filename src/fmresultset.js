741b8c73f920c74bb394f7d5d24865748dacc93d
Todd Geist <todd@geistinteractive.com>
2016-12-07 06:08:51 -0800

    refactor

--- src/fmresultset.js
+++ src/fmresultset.js
@@ -1,75 +1,98 @@
 'use strict';
 /**
- * Created by toddgeist on 4/23/16.
+ * converts FMS XML response in to normalized JSON
  */
-
-
 
 
 const parseString = require('./parseString');
 
+
+
+/**
+ * normalize field definitions
+ * @param json
+ * @returns {{}}
+ */
+const normalizeFieldDefs = (json)=>{
+  const fieldDefinations = json.fmresultset.metadata[0]['field-definition']
+
+  const fields = {}
+  fieldDefinations.map(field=>{
+    field = field.$;
+    fields[field.name] = {
+      autoEnter: field['auto-enter'] === 'yes',
+      timeOfDay: field['time-of-day'] === 'yes',
+      notEmpty: field['not-empty'] === 'yes',
+      numericOnly: field['numeric-only'] === 'yes',
+      fourDigitYear: field['four-digit-year'] === 'yes',
+      global: field['global'] === 'yes',
+      maxRepeat: field['max-repeat'] === 'yes'
+    };
+  });
+
+  return fields
+};
+
+
+
+/**
+ * normalize the records array out of the response
+ * @param json
+ * @returns {Array|*|{}}
+ */
+const normalizeRecords = (json) => {
+  const resultSet = json.fmresultset.resultset[0];
+
+  return resultSet.record.map(record => {
+    const recObj = {};
+    recObj['modId'] = record.$['mod-id'];
+    recObj['recId'] = record.$['record-id'];
+    const arrayOfFieldData = record.field
+    arrayOfFieldData.map(field=>{
+      recObj[field.$.name] = field.data[0]
+    });
+    return recObj
+  });
+
+};
+
+
+
+/**
+ * normalizes JSON
+ * @param json
+ * @returns {{errorCode: Number, product: *, datasource: *}}
+ */
+const formatJSON= (json)=>{
+  const product = json.fmresultset.product[0].$;
+  const datasource = json.fmresultset.datasource[0].$;
+  const errorCode = parseInt(json.fmresultset.error[0].$.code);
+
+  const result = {
+    errorCode,
+    product,
+    datasource
+  };
+
+  if(errorCode !== 0){
+    return result
+  }
+
+  result.records = normalizeRecords(json);
+  result.fields = normalizeFieldDefs(json);
+  return result
+};
+
+
+
+/**
+ * converts FMS XML response into normalized JSON
+ * @param xml
+ * @returns {*}
+ */
 module.exports = (xml)=> {
 
   return parseString(xml)
-    .then(json=>{
-      const product = json.fmresultset.product[0].$;
-      const datasource = json.fmresultset.datasource[0].$;
+    .then(formatJSON);
 
-
-
-      const errorCode = parseInt(json.fmresultset.error[0].$.code);
-
-
-
-      const result = {
-        errorCode,
-        product,
-        datasource
-      };
-
-      if(errorCode !== 0){
-        return result
-      }
-
-
-      const fieldDefinations = json.fmresultset.metadata[0]['field-definition']
-
-      const fields = {}
-      fieldDefinations.map(field=>{
-        field = field.$
-        const newField = {
-          autoEnter: field['auto-enter'] === 'yes',
-          timeOfDay: field['time-of-day'] === 'yes',
-          notEmpty: field['not-empty'] === 'yes',
-          numericOnly: field['numeric-only'] === 'yes',
-          fourDigitYear: field['four-digit-year'] === 'yes',
-          global: field['global'] === 'yes',
-          maxRepeat: field['max-repeat'] === 'yes'
-        }
-
-        fields[field.name] = newField;
-
-      });
-
-
-      const resultSet = json.fmresultset.resultset[0];
-      const records = resultSet.record.map(record => {
-        const recObj = {};
-        recObj['modId'] = record.$['mod-id'];
-        recObj['recId'] = record.$['record-id'];
-        const arrayOfFieldData = record.field
-        arrayOfFieldData.map(field=>{
-          recObj[field.$.name] = field.data[0]
-        });
-        return recObj
-
-      });
-
-      result.records = records;
-      result.fields = fields;
-      console.log(result)
-
-      return result
-    })
-
-}
+};
